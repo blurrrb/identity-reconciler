2a6462bfcba987e9e1ac841bca4222cdb524d2e7
Aakash Singh <17aakashsingh1999@gmail.com>
2023-06-11 20:58:53 +0530

    handle bad request

--- src/hono-cloudflare/controller.ts
+++ src/hono-cloudflare/controller.ts
@@ -1,3 +1,4 @@
+import { ErrInvalidReconciliationRequest } from "../reconciliation";
 import { getAppState } from "./app_state";
 import Ctx from "./ctx";
 
@@ -8,10 +9,18 @@ export async function identify(c: Ctx) {
     email?: string;
     phoneNumber?: string;
   };
-
-  return c.json(
-    await appState.reconciliationService.reconcileLinks(email, phoneNumber)
-  );
+  try {
+    return c.json(
+      await appState.reconciliationService.reconcileLinks(email, phoneNumber)
+    );
+  } catch (err) {
+    if (err === ErrInvalidReconciliationRequest) {
+      return c.text(
+        "bad request. atleast one of email or phone or both are required",
+        { status: 400 }
+      );
+    }
+  }
 }
 
 export async function ping(c: Ctx) {
