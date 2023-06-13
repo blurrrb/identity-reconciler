ae6af63a10f9c1ba07083185a2f7a68f1715d78a
Aakash Singh <17aakashsingh1999@gmail.com>
2023-06-13 16:49:17 +0530

    fix neon db connectivity issue

--- src/drizzle/store/neon.ts
+++ src/drizzle/store/neon.ts
@@ -1,9 +1,9 @@
-import { Client } from "@neondatabase/serverless";
+import { Pool } from "@neondatabase/serverless";
 import { NeonDatabase, drizzle } from "drizzle-orm/neon-serverless";
 
 export async function NewNeonPostgres(
   databaseUrl: string
 ): Promise<NeonDatabase> {
-  const pool = new Client({ connectionString: databaseUrl });
+  const pool = new Pool({ connectionString: databaseUrl });
   return drizzle(pool);
 }
